2a6d8116a92687932df57d312b1edd9f0930348c
Laurens1234 <laurensekke@gmail.com>
2026-03-21 23:45:28 +0100

    courier

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -812,7 +812,7 @@ leaders = [
     {
         "name": "Courier",
         "abilities": (
-            "*Swift.* At the start of each chapter, put 3 agents on planets with no loyal pieces. When you **move** into one, return the agent and gain a resource matching that planet.\n"
+            "*Swift.* At the start of each chapter, place 3 agents on planets with no loyal pieces. When you **move** into one, return the agent and gain a resource matching that planet.\n"
             "*Careless.* You cannot gain bonus city Power if you have not returned all agents on the board."
         ),
         "resources": ["Fuel", "Fuel"],
