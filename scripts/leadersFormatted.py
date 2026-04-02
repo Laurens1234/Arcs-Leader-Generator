34acb4eb9faef9fa66f8de5be145981b33015c18
Laurens1234 <laurensekke@gmail.com>
2026-04-02 20:42:39 +0200

    necro

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -97,7 +97,7 @@ leaders = [
     {
         "name": "Necromancer",
         "abilities": (
-            "*Arising.* After you destroy a piece in **battle**, you may place a matching fresh loyal piece there.\n"
+            "*Arising.* After **any** **battle**, for each piece you destroy, you may place a matching fresh loyal piece there.\n"
             "*Gravebound.* In **setup**, damage both of your buildings. You cannot **build** fresh pieces, place them damaged instead."
         ),
         "resources": ["Psionic", "Weapon"],
