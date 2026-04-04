a5fd431236abb161618ab97603a49418920c0502
Laurens1234 <laurensekke@gmail.com>
2026-04-04 19:00:00 +0200

    ass

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -947,7 +947,7 @@ leaders = [
         "name": "Assasin",
         "abilities": (
             "*Lethal.* After you **influence**, if you have the most agents on a card, destroy one rival agent on it.\n"
-            "*Doomed.* If a Rival has as many or more agents on a card than you, you can only **influence** those cards."
+            "*Outmatched.* If a Rival has as many or more agents on a card than you, you can only **influence** those cards."
         ),
         "resources": ["Weapon", "Psionic"],
         "setup": {
