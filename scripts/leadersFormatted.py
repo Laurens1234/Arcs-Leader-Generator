0da52cd1c643228392a46eb8ee218330789adaca
Laurens1234 <laurensekke@gmail.com>
2026-04-07 22:34:43 +0200

    egoist

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -1127,7 +1127,7 @@ leaders = [
         "name": "Egoist",
         "abilities": (
             "*Willful.* When you Pivot, you may take any action except those on your played card.\n"
-            "*Inflexible.* You can only Pivot with cards of the lead suit."
+            "*Inflexible.* You can only Copy with cards of the lead suit."
         ),
         "resources": ["Psionic", "Fuel"],
         "setup": {
