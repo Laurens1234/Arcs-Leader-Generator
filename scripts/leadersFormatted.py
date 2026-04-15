4567fa50d74d5556f74d576fc9084e34d9f84c41
Laurens1234 <89664514+Laurens1234@users.noreply.github.com>
2026-04-15 15:52:15 +0200

    Update Extortioner abilities and resources

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -1086,6 +1086,7 @@ leaders = [
         "abilities": (
             "*Shrewd.* **Prelude:** Once per turn, you may return 3 Trophies/Captives of a Rival to steal one of their resources.\n"
             "*Vindictive.* In scoring, when you win first place, you must return a Trophy or Captive, if you choose not to, gain no Power."
+        ),
         "resources": ["Psionic", "Weapon"],
         "setup": {
             "A": {"ships": 3, "building": "city"},
