223fc1ef7276df40323a02457e2807a0ca8a98be
Laurens1234 <89664514+Laurens1234@users.noreply.github.com>
2026-04-15 18:01:22 +0200

    Update Seer abilities descriptions in leadersFormatted.py

--- scripts/leadersFormatted.py
+++ scripts/leadersFormatted.py
@@ -163,8 +163,8 @@ leaders = [
     {
         "name": "Seer",
         "abilities": (
-            "*Prescient.* After you discard a Guild card using its **Prelude** action, you may take an action on the **Lead** **card**.\n"
-            "*Narrow-minded.* You **cannot** take more than 2 different types of standard actions each turn."
+            "*Prescient.* After you discard a Guild card with its **Prelude** action, you may take an action on the **Lead** **card**.\n"
+            "*Narrow-minded.* You **cannot** discard a Guild card with it's Prelude action unless you're Leading or Surpassing."
         ),
         "resources": ["Psionic", "Psionic"],
         "setup": {
